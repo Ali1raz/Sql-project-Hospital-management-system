9bd6f8e51a0ecae2c005b40525b96a3b90cb407a
Ali Raza <alirazakhalid1129@gmail.com>
2024-12-22 13:11:37 +0500

    Queries 1.1.1

--- Queries.sql
+++ Queries.sql
@@ -4,3 +4,34 @@ select * from Doctors;
 select * from Patients;
 select * from MedicalRecords;
 select * from Appointments;
+
+-- add col in tables:
+--alter table doctors
+--add city nvarchar(40);
+
+-- modify column:
+--alter table doctors
+--alter column city nvarchar(10);
+
+-- droping column:
+--alter table doctors drop column city;
+
+-- select 
+select full_name, specialty from Doctors
+where doctor_id = 2;
+
+select * from Patients where date_of_birth < '1993-1-1';
+select * from Patients where date_of_birth = '1993-%';
+
+-- update rows:
+--update Patients 
+--set date_of_birth = '1993-01-01',
+--	full_name = 'Ripper'
+--where patient_id = 6;
+
+-- delete a row
+--delete from MedicalRecords
+--where record_id = 1;
+
+
+
